c20e3090d39a947a18f12e7d61085a7a7e8f4b26
김준민/SG NCF서버팀 <junmkim@smilegate.com>
2026-02-13 10:18:08 +0900

    Update: reflect only my changes

--- src/ICommandExecutor.cs
+++
@@ -1,9 +0,0 @@
-// ICommandExecutor.cs
-// 명령 실행 인터페이스
-namespace YoutubeTogether
-{
-    public interface ICommandExecutor
-    {
-        void Execute(string command, string argument = null);
-    }
-}
